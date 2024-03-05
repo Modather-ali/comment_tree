f0338409df096a3142f7f97bc1ea6a7e8ebb2161
Modather Ali <modather0ali@gmail.com>
2024-03-05 21:15:33 +0200

    Update comment.dart

--- lib/data/comment.dart
+++ lib/data/comment.dart
@@ -3,15 +3,18 @@ class Comment {
   static const TAG = 'Comment';
 
   String commentId;
+  String commenterId;
   String? avatar;
   String? userName;
   String? content;
   DateTime commentDate;
 
-  Comment(
-      {required this.commentId,
-      required this.avatar,
-      required this.userName,
-      required this.content,
-      required this.commentDate});
+  Comment({
+    required this.commentId,
+    required this.commenterId,
+    required this.avatar,
+    required this.userName,
+    required this.content,
+    required this.commentDate,
+  });
 }
